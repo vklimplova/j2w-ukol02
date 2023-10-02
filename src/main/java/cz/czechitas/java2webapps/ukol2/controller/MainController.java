363cd0ab4e9a4ad707213b026d7a039e46e5f6a4
veruklimpl <veronika.klimplova@seznam.cz>
2023-10-02 23:12:53 +0200

    vytvoření controlleru, řešení citátů, nefunguje propojené s index.ftlh

---
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -0,0 +1,46 @@
+package cz.czechitas.java2webapps.ukol2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+public class MainController {
+
+        private static List<String> readAllLines(String resource)throws IOException {
+            //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
+            ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
+
+            //Pomocí metody getResourceAsStream() získáme z classloaderu InpuStream, který čte z příslušného souboru.
+            //Následně InputStream převedeme na BufferedRead, který čte text v kódování UTF-8
+            try(InputStream inputStream=classLoader.getResourceAsStream("citaty.txt");
+                BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+
+                //Metoda lines() vrací stream řádků ze souboru. Pomocí kolektoru převedeme Stream<String> na List<String>.
+                return reader
+                        .lines()
+                        .collect(Collectors.toList());
+            }
+        }
+    @GetMapping ("/")
+    public ModelAndView citaty() throws IOException {
+        List<String> citaty = readAllLines("citaty.txt");
+        ModelAndView result = new ModelAndView("index");
+        result.addObject("citat", citaty);
+        return result;
+    }
+}
+
+
+
+
+
