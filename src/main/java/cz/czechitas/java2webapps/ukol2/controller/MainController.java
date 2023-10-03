b311b5e61df70895167af591c541937a9b675c08
veruklimpl <veronika.klimplova@seznam.cz>
2023-10-03 22:19:06 +0200

    přidání obrázků

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -9,33 +9,38 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
+
 
 @Controller
 public class MainController {
 
-        private static List<String> readAllLines(String resource)throws IOException {
-            //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
-            ClassLoader classLoader=Thread.currentThread().getContextClassLoader();
 
-            //Pomocí metody getResourceAsStream() získáme z classloaderu InpuStream, který čte z příslušného souboru.
-            //Následně InputStream převedeme na BufferedRead, který čte text v kódování UTF-8
-            try(InputStream inputStream=classLoader.getResourceAsStream("citaty.txt");
-                BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))){
+        private static List<String> readAllLines()throws IOException{
+        //Soubory z resources se získávají pomocí classloaderu. Nejprve musíme získat aktuální classloader.
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
 
-                //Metoda lines() vrací stream řádků ze souboru. Pomocí kolektoru převedeme Stream<String> na List<String>.
-                return reader
-                        .lines()
-                        .collect(Collectors.toList());
-            }
+        //Pomocí metody getResourceAsStream() získáme z classloaderu InpuStream, který čte z příslušného souboru.
+        //Následně InputStream převedeme na BufferedRead, který čte text v kódování UTF-8
+        try (InputStream inputStream = classLoader.getResourceAsStream("citaty.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+
+            //Metoda lines() vrací stream řádků ze souboru. Pomocí kolektoru převedeme Stream<String> na List<String>.
+            return reader
+                    .lines()
+                    .collect(Collectors.toList());
         }
-    @GetMapping ("/")
+    }
+    private final Random random = new Random();
+    @GetMapping("/")
     public ModelAndView citaty() throws IOException {
-        List<String> citaty = readAllLines("citaty.txt");
+        int nahodneCislo = random.nextInt(1) + 1;
+
         ModelAndView result = new ModelAndView("index");
-        result.addObject("citat", citaty);
+        result.addObject("citat", readAllLines());
+        result.addObject("obrazek",String.format("/static/images/obrazek-%d.jpg", nahodneCislo));
         return result;
     }
 }
